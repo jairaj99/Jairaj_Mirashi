e5f760d4c0a064b72768b86b8c67f8099b9ef9cc
Jairaj Mirashi <109090263+jairaj99@users.noreply.github.com>
2024-08-07 11:53:18 +0530

    Add files via upload

---
+++ D_FF/rtl/dff.v
@@ -0,0 +1,29 @@
+module dff (clock,
+	   reset,
+	   d_in,
+	   Q_out,
+	   Qb_out);
+
+   //Step1 : Declare Port Directions
+input clock,reset,d_in;
+output reg Q_out;
+output Qb_out;
+
+
+   /*Understand the Behaviour of D flip-flop &
+   check the coding style of synchronous reset*/
+
+   always@(posedge clock)
+      begin
+	 if(reset)
+	    Q_out <= 1'b0;
+	 else
+	    Q_out <= d_in;
+      end
+
+   //Step2 : Write the logic for Qbar				
+	assign Qb_out=~Q_out;
+	
+endmodule          
+
+
