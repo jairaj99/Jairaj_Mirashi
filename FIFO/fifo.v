4ea89995621638e862123d518e783c3bd2516877
Jairaj Mirashi <109090263+jairaj99@users.noreply.github.com>
2024-08-13 10:52:00 +0530

    fifo.v

--- FIFO/fifo.v
+++ FIFO/fifo.v
@@ -10,7 +10,47 @@ integer i;
 
 always@(posedge clk)
 if(rst)
+beginmodule fifo(
+    input clk,
+    input rst,
+    input we,
+    input re,
+    input [7:0] data_in,
+    output reg [7:0] data_out,
+    output full,
+    output empty
+);
+
+reg [3:0] rd_pt, wr_pt;
+reg [7:0] mem[15:0]; // FIFO depth is 16, so indexing is from 0 to 15
+integer i;
+
+always @(posedge clk or posedge rst)
 begin
+    if (rst) begin
+        data_out <= 0;
+        wr_pt <= 0;
+        rd_pt <= 0;
+        for (i = 0; i < 16; i = i + 1)
+            mem[i] <= 0;
+    end
+    else begin
+        if (we && !full) begin
+            mem[wr_pt] <= data_in;
+            wr_pt <= wr_pt + 1;
+        end
+        if (re && !empty) begin
+            data_out <= mem[rd_pt];
+            rd_pt <= rd_pt + 1;
+        end
+    end
+end
+
+assign empty = (wr_pt == rd_pt);
+assign full = (wr_pt == 15 && !re && we);
+
+endmodule
+
 data_out <= 0;
 wr_pt <=0;
 rd_pt <=0;
