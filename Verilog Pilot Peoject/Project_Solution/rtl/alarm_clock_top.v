f271fffbea4deb1d484c235e0dab758ed01becb8
Jairaj Mirashi <109090263+jairaj99@users.noreply.github.com>
2024-08-13 13:59:37 +0530

    Update alarm_clock_top.v

--- Verilog Pilot Peoject/Project_Solution/rtl/alarm_clock_top.v
+++ Verilog Pilot Peoject/Project_Solution/rtl/alarm_clock_top.v
@@ -1,31 +1,4 @@
-/********************************************************************************************
 
-Copyright 2018-2019 - Maven Silicon Softech Pvt Ltd. All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-It is considered a trade secret and is not to be divulged or used by parties who 
-have not received written authorization from Maven Silicon Softech Pvt Ltd.
-
-Maven Silicon Softech Pvt Ltd
-Bangalore - 560076
-
-Webpage: www.maven-silicon.com
-
-Filename:	alarm_clock_top.v   
-
-Description:	This is the top module of Alarm clock.
-                It has instantiated all the sub-modules 
-		which are connected via internal wires .
-
-Date:		01/05/2018
-
-Author:		Maven Silicon
-
-Email:	        online@maven-silicon.com	
-
-Version:	1.0
-
-*********************************************************************************************/
 module alarm_clock_top(clock,
 	               key,
 		       reset,
