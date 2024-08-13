20333ed1e272425ff42f7df9cd451f025aa8addb
Jairaj Mirashi <109090263+jairaj99@users.noreply.github.com>
2024-08-13 14:00:26 +0530

    Update counter.v

--- Verilog Pilot Peoject/Project_Solution/rtl/counter.v
+++ Verilog Pilot Peoject/Project_Solution/rtl/counter.v
@@ -1,30 +1,4 @@
-/********************************************************************************************
 
-Copyright 2018-2019 - Maven Silicon Softech Pvt Ltd. All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-It is considered a trade secret and is not to be divulged or used by parties who 
-have not received written authorization from Maven Silicon Softech Pvt Ltd.
-
-Maven Silicon Softech Pvt Ltd
-Bangalore - 560076
-
-Webpage: www.maven-silicon.com
-
-Filename:	counter.v   
-
-Description:	This is a functional description of Alarm clock
-                counter unit .It generates the counting sequence for current time.
-
-Date:		01/05/2018
-
-Author:		Maven Silicon
-
-Email:		online@maven-silicon.com
-
-Version:	1.0
-
-*********************************************************************************************/
 
 module counter (clk,
 	        reset,
