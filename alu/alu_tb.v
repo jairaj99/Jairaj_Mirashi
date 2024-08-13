61e58e37512944d9137585dd6453b81e3e4b6368
Jairaj Mirashi <109090263+jairaj99@users.noreply.github.com>
2024-08-13 13:56:56 +0530

    alu_tb.v

--- alu/alu_tb.v
+++ alu/alu_tb.v
@@ -1,25 +1,4 @@
-/********************************************************************************************
-Copyright 2019 - Maven Silicon Softech Pvt Ltd. 
- 
-All Rights Reserved.
 
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-
-It is not to be shared with or used by any third parties who have not enrolled for our paid training 
-
-courses or received any written authorization from Maven Silicon.
-
-
-Webpage     :      www.maven-silicon.com
-
-Filename    :	   alu_tb.v   
-
-Description :      Arithmetic Logic Unit Design Testbench
-
-Author Name :      Susmita
-
-Version     :      1.0
-*********************************************************************************************/
 module alu_tb();
     
    //Testbench global variables
