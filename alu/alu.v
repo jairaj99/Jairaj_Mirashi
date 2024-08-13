5dcf88836aa7bf6e5e5afa45bb9a7571d148b26d
Jairaj Mirashi <109090263+jairaj99@users.noreply.github.com>
2024-08-13 13:56:12 +0530

    alu.v

--- alu/alu.v
+++ alu/alu.v
@@ -1,25 +1,4 @@
-/********************************************************************************************
-Copyright 2019 - Maven Silicon Softech Pvt Ltd. 
 
-All Rights Reserved.
-
-This source code is an unpublished work belongs to Maven Silicon Softech Pvt Ltd.
-
-It is not to be shared with or used by any third parties who have not enrolled for our paid training 
-
-courses or received any written authorization from Maven Silicon.
-
-
-Webpage     :      www.maven-silicon.com
-
-Filename    :	   alu.v   
-
-Description :      Arithmetic Logic Unit Design
-
-Author Name :      Susmita
-
-Version     :      1.0
-*********************************************************************************************/
 
 module alu(input [7:0]a_in,b_in,
            input [3:0]command_in,
